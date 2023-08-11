5f35b8196220e87f748f369ee96bf506660e73f2
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-08-11 18:54:46 +0300

    DockerDemo

--- RealTimeAppsDemo/src/07. SignalRChat/Hubs/ChatHub.cs
+++ RealTimeAppsDemo/src/07. SignalRChat/Hubs/ChatHub.cs
@@ -1,20 +1,20 @@
 ﻿namespace SignalRChat.Hubs
 {
-    using System.Threading.Tasks;
+	using System.Threading.Tasks;
 
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.SignalR;
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.SignalR;
 
-    using SignalRChat.Models.Chat;
+	using SignalRChat.Models.Chat;
 
-    [Authorize]
-    public class ChatHub : Hub
-    {
-        public async Task Send(string message)
-        {
-            await this.Clients.All.SendAsync(
-                "NewMessage",
-                new Message { User = this.Context.User.Identity.Name, Text = message, });
-        }
-    }
+	[Authorize]
+	public class ChatHub : Hub
+	{
+		public async Task Send(string message)
+		{
+			await this.Clients.All.SendAsync(
+				"NewMessage",
+				new Message { User = this.Context.User.Identity.Name, Text = message, });
+		}
+	}
 }
